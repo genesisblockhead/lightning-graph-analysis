6938b08bf9abbc513c5a5ed72f6c3c447a58b343
genesisblockhead <genesisblockhead@protonmail.com>
2022-08-02 13:45:17 +0000

    add indexes

--- main.sh
+++ main.sh
@@ -8,18 +8,19 @@ mkdir -p $SOURCES $NEO4J
 tee >(./channels.sh > ${SOURCES}/channels.csv) \
     >(./nodes.sh > ${SOURCES}/nodes.csv) \
     | cat > ${SOURCES}/graph.json
+cp schema.cypher data.cypher ${SOURCES}/
 
 docker run \
   --volume=$(pwd)/${NEO4J}:/data \
   --volume=$(pwd)/${SOURCES}:/var/lib/neo4j/import \
   neo4j:latest bin/neo4j-admin import --nodes import/nodes.csv --relationships import/channels.csv
 
-CYPHER_0='create index alias_index for (n:Node) on (n.alias)'
-CYPHER_1='create index pub_key_index for (n:Node) on (n.pub_key)'
 docker run -p 7474:7474 -p 7687:7687 \
   --volume=$(pwd)/${NEO4J}:/data \
+  --volume=$(pwd)/${SOURCES}:/var/lib/neo4j/import \
   --env NEO4JLABS_PLUGINS='["apoc", "graph-data-science"]' \
   --env NEO4J_AUTH=none \
-  --env apoc.initializer.neo4j.cypher.0="${CYPHER_0}" \
-  --env apoc.initializer.neo4j.cypher.1="${CYPHER_1}" \
+  --env apoc.import.file.enabled=true \
+  --env apoc.initializer.neo4j.0='CALL apoc.cypher.runSchemaFile("file://import/schema.cypher")' \
+  --env apoc.initializer.neo4j.1='CALL apoc.cypher.runFile("file://import/data.cypher")' \
   neo4j:latest
---
+++ schema.cypher
@@ -0,0 +1,3 @@
+
+CREATE INDEX alias_index FOR (n:Node) ON (n.alias);
+CREATE INDEX pub_key_index FOR (n:Node) ON (n.pub_key);
